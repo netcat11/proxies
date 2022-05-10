26ca9b18f9065307c245b858816b53af0323a53a
netcat11 <105028201+netcat11@users.noreply.github.com>
2022-05-11 01:55:21 +0800

    Update cip.py

--- cip.py
+++ cip.py
@@ -6,7 +6,5 @@ def getrandomip():
     reg=r'(\d+\.\d+\.\d+\.\d+)\s*:(\d+)'
     ip_list=re.findall(reg,content)
     random_ip=random.choice(ip_list)
-    proxies={'http':random_ip[0]+':'+random_ip[1],'https':random_ip[0]+':'+random_ip[1]}
+    proxies={'http':random_ip[0]+':'+random_ip[1]}
     return proxies
-a=getrandomip()
-print(a)
